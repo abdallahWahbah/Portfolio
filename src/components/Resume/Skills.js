5297480e35e6ed8aa08310d90cfca382f8633722
Abdallah Wahbah <40547258+abdallahWahbah@users.noreply.github.com>
2024-04-17 07:40:13 +0300

    ----

--- src/components/Resume/Skills.js
+++ src/components/Resume/Skills.js
@@ -7,25 +7,28 @@ const Skills = ({skills}) =>
         <ul className='skills__list'>
             {skills?.map(skill => (
                 <li key={skill.title} className="skill__item">
-                    {/* <h5 className='skill__title'>{skill.title}</h5>
-                    <div className='skill__percentage--container'>
-                        <h6 className='skill__percentage'>{`${skill.percentage}%`}</h6>
-                        <div className='skill__line'>
-                            <span className='skill__line--background'/>
-                            <span className='skill__line--above' style={{width: `${skill.percentage}%`}}/>
-                        </div>
-                    </div> */}
+                    <div className='skill__list--item'>
+                        {/* <h5 className='skill__title'>{skill.title}</h5>
+                        <div className='skill__percentage--container'>
+                            <h6 className='skill__percentage'>{`${skill.percentage}%`}</h6>
+                            <div className='skill__line'>
+                                <span className='skill__line--background'/>
+                                <span className='skill__line--above' style={{width: `${skill.percentage}%`}}/>
+                            </div>
+                        </div> */}
 
-                    {/* <img className='skill__image' src={skill.image} alt={skill.title}/>  */}
-                
-                    <div className='skill__icon--container'>
-                        {skill.icon && (skill.icon)}
-                    </div>
-                    <div className='skill__data--container'>
-                        <h4 className='skill__name'>{skill.title}</h4>
-                        <span className='skill__level'>{skill.level}</span>
+                        {/* <img className='skill__image' src={skill.image} alt={skill.title}/>  */}
+                    
+                        <div className='skill__icon--container'>
+                            {skill.icon && (skill.icon)}
+                        </div>
+                        <div className='skill__data--container'>
+                            <h4 className='skill__name'>{skill.title}</h4>
+                            <span className='skill__level'>{skill.level}</span>
+                        </div>
                     </div>
                 </li>
+
             ))}
         </ul>
     )
